5f0667e9ebcd9081894385270af2ecafac3e71b1
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-09-29 16:23:15 -0500

    Created Interface and Panel Scripts
    Each panel has its own script and has inherited the IPanel Interface. The plan is every time the user presses the submit button in the panels, the process info method will be called.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/assets/Scripts/Panels/SelectPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPanel : MonoBehaviour, IPanel
+{
+    public void ProcessInfo()
+    {
+        
+    }
+}
