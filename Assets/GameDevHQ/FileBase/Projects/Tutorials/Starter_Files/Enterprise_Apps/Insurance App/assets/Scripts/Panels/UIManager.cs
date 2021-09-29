41a417c2eee512aed0cd8f7379b5d938e627be8b
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-09-29 18:14:07 -0500

    Created UI manager and Case class
    The UI manager will keep track of the case information. When the user goes through the panels, the submitted information will be populated in this class.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Enterprise_Apps/Insurance App/assets/Scripts/Panels/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("UI Manager is Null");
+            }
+
+            return _instance;
+        }
+    }
+
+    public Case activeCase;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
