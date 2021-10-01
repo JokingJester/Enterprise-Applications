6ffaef36c087aae9b50b316548ad5872c2b87111
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-09-30 23:03:05 -0500

    Photo Taken Panel Complete
    You are now able to take a photo and it will be displayed on the app.

--- Assets/Scripts/Panels/TakePhotoPanel.cs
+++ Assets/Scripts/Panels/TakePhotoPanel.cs
@@ -5,16 +5,44 @@ using UnityEngine.UI;
 
 public class TakePhotoPanel : MonoBehaviour, IPanel
 {
-    public Text caseNumberText;
-    public RawImage photoTaken;
-    public InputField photoNotes;
+	public Text caseNumberText;
+	public RawImage photoTaken;
+	public InputField photoNotes;
 
-    private void OnEnable()
+	private void OnEnable()
+	{
+		caseNumberText.text = "CASE NUMBER: " + UIManager.Instance.activeCase.caseID;
+	}
+	public void ProcessInfo()
+	{
+		UIManager.Instance.activeCase.photoTaken = photoTaken;
+		UIManager.Instance.activeCase.photoNotes = photoNotes.text;
+	}
+
+	public void TakePictureButton()
     {
-        caseNumberText.text = "CASE NUMBER: " + UIManager.Instance.activeCase.caseID;
+		TakePicture(512);
     }
-    public void ProcessInfo()
-    {
-        
-    }
+
+    private void TakePicture(int maxSize)
+	{
+		NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+		{
+			Debug.Log("Image path: " + path);
+			if (path != null)
+			{
+				// Create a Texture2D from the captured image
+				Texture2D texture = NativeCamera.LoadImageAtPath(path, maxSize);
+				if (texture == null)
+				{
+					Debug.Log("Couldn't load texture from " + path);
+					return;
+				}
+
+				photoTaken.texture = texture;
+				photoTaken.gameObject.SetActive(true);
+			}
+		}, maxSize);
+
+	}
 }
