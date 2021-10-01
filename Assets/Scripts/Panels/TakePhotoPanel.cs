335e93d52195280277ae86b57d4c143237c17826
JokingJester <81639209+JokingJester@users.noreply.github.com>
2021-09-30 21:42:59 -0500

    Installed Camera Plugin + API Key Changed
    I installed the camera plugin that will allow the user to take a photo and display the photo on the app. On accident on the last commit I kept the API key in the files. That key has been deleted and is now changed.

--- Assets/Scripts/Panels/TakePhotoPanel.cs
+++ Assets/Scripts/Panels/TakePhotoPanel.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 
 public class TakePhotoPanel : MonoBehaviour, IPanel
 {
+    public Text caseNumberText;
     public RawImage photoTaken;
     public InputField photoNotes;
+
+    private void OnEnable()
+    {
+        caseNumberText.text = "CASE NUMBER: " + UIManager.Instance.activeCase.caseID;
+    }
     public void ProcessInfo()
     {
-
+        
     }
 }
